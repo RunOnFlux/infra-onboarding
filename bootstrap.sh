8e5ee293f1a6d679282dbe1be5ae95c1f1c33cf8
David White <david@runonflux.io>
2024-04-19 16:32:20 +0100

    Fix script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
+[[ $(id -u) -eq 0 ]] || exec sudo /bin/bash -c "$(printf '%q ' "$BASH_SOURCE" "$@")"
+
 USER=ansible
 
 PUBKEY=$(curl -s https://raw.githubusercontent.com/RunOnFlux/infra-onboarding/master/deployment_pubkey)
 
-[[ $(id -u) -eq 0 ]] || exec sudo /bin/bash -c "$(printf '%q ' "$BASH_SOURCE" "$@")"
+if getent passwd $USER > /dev/null 2>&1; then
+  userdel $USER
+  rm -rf /home/$USER
+fi
 
-useradd -m --system --shell /bin/bash --groups sudo $USER
+useradd --create-home --system --shell /bin/bash --groups sudo $USER
 
 echo "$USER ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/$USER
 
 mkdir /home/$USER/.ssh
 
-echo $PUBKEY > /home/$USER/.ssh/authorized_keys
+echo "${PUBKEY} ansible" > /home/$USER/.ssh/authorized_keys
 
 chown $USER:$USER /home/$USER/.ssh/authorized_keys
